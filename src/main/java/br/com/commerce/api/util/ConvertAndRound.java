fb886e0acefffaeb06ea1086da7e525a6988942a
Emerson Pereira da Silva <emerson.pereira@compasso.com.br>
2023-10-12 10:45:55 -0300

    Fix: ConvertAndRound class cannot be instantiated

--- src/main/java/br/com/commerce/api/util/ConvertAndRound.java
+++ src/main/java/br/com/commerce/api/util/ConvertAndRound.java
@@ -5,6 +5,10 @@ import java.math.RoundingMode;
 
 public class ConvertAndRound {
 
+    private ConvertAndRound() {
+        throw new IllegalStateException("ConvertAndRound class cannot be instantiated");
+    }
+
     public static double convert(Double value) {
         BigDecimal bd = BigDecimal.valueOf(value);
         bd = bd.setScale(2, RoundingMode.HALF_UP);
